d1416301a40131d6b31e3e14d2297ccd8e183fa5
Abdul Halim <a.halimics@gmail.com>
2020-07-30 18:10:00 +0600

    Question Model & migration created

---
+++ app/Question.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+class Question extends Model
+{
+    protected $fillable = ['title', 'body'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
